fcb280ec758a0e222abc1e2b7c03010e6ece510f
simontbone <59731760+simontbone@users.noreply.github.com>
2026-09-21 14:42:55 -0500

    Fix plugin packaging output paths

--- scripts/package.mjs
+++ scripts/package.mjs
@@ -4,5 +4,5 @@ await mkdir('dist', {recursive: true})
 for (const file of ['dist/antelope-wallets-wordpress.zip','dist/antelope_wallets-drupal.zip']) {
   await rm(file, {force: true})
 }
-execFileSync('zip', ['-qr', '../../dist/antelope-wallets-wordpress.zip', 'antelope-wallets'], {cwd: 'wordpress', stdio: 'inherit'})
-execFileSync('zip', ['-qr', '../../dist/antelope_wallets-drupal.zip', 'antelope_wallets'], {cwd: 'drupal', stdio: 'inherit'})
+execFileSync('zip', ['-qr', '../dist/antelope-wallets-wordpress.zip', 'antelope-wallets'], {cwd: 'wordpress', stdio: 'inherit'})
+execFileSync('zip', ['-qr', '../dist/antelope_wallets-drupal.zip', 'antelope_wallets'], {cwd: 'drupal', stdio: 'inherit'})
